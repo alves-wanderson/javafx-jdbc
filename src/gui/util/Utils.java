79f12e980e6d0631217eac7f88c3c9ad79eeffe6
Wanderson Alves <alves.wanderson@outlook.com>
2021-09-15 20:23:02 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -29,6 +29,15 @@ public class Utils {
 		}
 	}
 	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
+	
 	public static <T> void formatTableComlumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
 			TableCell<T, Date> cell = new TableCell<T, Date>(){
